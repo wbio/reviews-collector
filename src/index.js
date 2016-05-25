858cbc78be0101781918882b9661ea63d1fb3795
wbio <bogeywm@gmail.com>
2016-05-25 14:18:01 -0400

    Modularizing

--- src/index.js
+++ src/index.js
@@ -5,71 +5,81 @@ const CollectorForAndroid = require('reviews-collector-android');
 const P = require('bluebird');
 const recorder = require('./recorder.js');
 
-const daysOfReviews = 2;	// The # of days of reviews to collect
-const now = new Date();
-const earliestReviewTime = now.setDate(now.getDate() - daysOfReviews);
 
-const collectors = [];
-const inserts = [];
+function Collector(options) {
+	if (!options.daysOfReviews) {
+		throw new Error('You must specify how many days of reviews to collect using the "daysOfReviews" property');
+	}
+	if (!options.ios && !options.android) {
+		throw new Error('You must include a list of apps to collect in the "ios" and/or "android" properties');
+	}
+	const daysOfReviews = options.daysOfReviews;
+	const now = new Date();
+	const earliestReviewTime = now.setDate(now.getDate() - daysOfReviews);
 
-// Create a collector for iOS
-collectors.push(new CollectorForiOS(
-	[ // List the IDs of the iOS apps you want to collect reviews for
-		'585027354',
-	],
-	{ checkBeforeContinue: true }
-));
+	const collectors = [];
+	const inserts = [];
 
-// Create a collector for Android
-collectors.push(new CollectorForAndroid(
-	[ // List the IDs of the Android apps you want to collect reviews for
-		'com.capitalone.mobile.wallet',
-	],
-	{ checkBeforeContinue: true }
-));
-
-begin();
-
-function begin() {
-	const numCollectors = collectors.length;
-	let numComplete = 0;
-	let i;
-
-	for (i = 0; i < numCollectors; i++) {
-		// Handle the review events
-		collectors[i].on('review', handleReview);
-		// Handle the page complete events
-		collectors[i].on('page complete', handlePageComplete);
-		// Handle the page complete events
-		collectors[i].on('done with apps', handleAllAppsComplete);
-		// Start collecting reviews
-		collectors[i].collect();
+	if (options.ios) {
+		// Create a collector for iOS
+		collectors.push(new CollectorForiOS(
+			options.ios,
+			{ checkBeforeContinue: true }
+		));
 	}
 
-	function handleReview(result) {
-		inserts.push(recorder.record(result));
+	if (options.android) {
+		// Create a collector for Android
+		collectors.push(new CollectorForAndroid(
+			options.android,
+			{ checkBeforeContinue: true }
+		));
 	}
 
-	function handlePageComplete(result) {
-		console.log(`${result.os}/${result.appId}: Parsed to ${result.firstReviewTime} (page ${result.pageNum})`);
-		// If we find a review from earlier than the first date we care about, stop collecting
-		if (result.firstReviewTime < earliestReviewTime) {
-			result.stop();
-		} else {
-			result.continue();
+	begin();
+
+	function begin() {
+		const numCollectors = collectors.length;
+		let numComplete = 0;
+		let i;
+
+		for (i = 0; i < numCollectors; i++) {
+			// Handle the review events
+			collectors[i].on('review', handleReview);
+			// Handle the page complete events
+			collectors[i].on('page complete', handlePageComplete);
+			// Handle the page complete events
+			collectors[i].on('done with apps', handleAllAppsComplete);
+			// Start collecting reviews
+			collectors[i].collect();
 		}
-	}
 
-	function handleAllAppsComplete(result) {
-		console.log(`>>> We have finished collecting reviews for the ${result.os} collector`);
-		numComplete++;
-		if (numComplete === numCollectors) {
-			console.log('>>>>> We have finished collecting reviews for all of the collectors');
-			// Wait for all of the inserts to finish and then exit
-			P.all(inserts)
-				.then(() => {
-					process.exit();
-				});
+		function handleReview(result) {
+			inserts.push(recorder.record(result));
+		}
+
+		function handlePageComplete(result) {
+			console.log(`${result.os}/${result.appId}: Parsed to ${result.firstReviewTime} (page ${result.pageNum})`);
+			// If we find a review from earlier than the first date we care about, stop collecting
+			if (result.firstReviewTime < earliestReviewTime) {
+				result.stop();
+			} else {
+				result.continue();
+			}
+		}
+
+		function handleAllAppsComplete(result) {
+			console.log(`>>> We have finished collecting reviews for the ${result.os} collector`);
+			numComplete++;
+			if (numComplete === numCollectors) {
+				console.log('>>>>> We have finished collecting reviews for all of the collectors');
+				// Wait for all of the inserts to finish and then exit
+				P.all(inserts)
+					.then(() => {
+						process.exit();
+					});
+			}
 		}
 	}
 }
+module.exports = Collector;
